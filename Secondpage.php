8c0f8177b61a506dd03b8254f76b12a52cab8dee
ranjanR0412 <ranjanrohit1607@gmail.com>
2024-11-15 12:03:26 +0530

    Add files via upload

---
+++ Secondpage.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.css">
+    
+</head>
+<body class="bg">
+
+    <h5 class="sph1"> <span>W</span>elcome <span>t</span>o <span>t</span>he <span class="h5spn">Family</span> </h5>
+   
+   <p class="para1"> Dear Bhabhi,</p>
+   <p class="para2"> I am very happy to announce that finallyyy, you're </p>
+    <p class="para3">officially our. And very soon you'll be comming to the</p> 
+    <p class="para4"> home that's waiting eagerly for you. And the most </p>
+    <p class="para5"> important part...I'm eagerly waiting for you...I want </p>
+   <p class="para6">  my partner in every crimes..Someone I can hold to keep</p>
+   <p class="para7">  my secrets everytime I do any kaand...Whenever I do </p>
+   <p class="para8">  anything wrong I know I'll have someone to watch my </p>
+   <p class="para9">back. I know i annoy you a lot but I promise to make</p>
+   <p class="para10">you smile in every serious situations and tease you</p>
+   <p class="para11"> in every possible way possible.</p>
+   <p class="para12">Your's Loving Handsome Devar</p>
+    <div class="img-hang"> 
+     <img src="pic_1.jpeg" id="img">
+    </div> 
+    <p class="para13"> Chalo...Don't get emotional now....Smile kro aur</p>
+    <p class="para14"> neeche click kro...I have a small gift for you</p>
+     <a class="button2" href="Thirdpage.php"><span>Abb click kro bhi</span></a>
+    <script type="text/javascript">
+        const image = document.getElementById('img');
+        image.style.display = 'none';
+        setTimeout(() =>{
+            image.style.display='block';
+        },51000);
+        setTimeout(() =>{
+            image.classList.add('show')
+        },51000);
+
+    </script>
+    
+</body>
+</html>
+
+
---
+++ ThirdPage.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.css">
+    
+</head>
+<body class="bg">
+
+    <h5 class="sph2"> Rona ni..Smile only..... </h5>
+   
+  
+   <p class="para15"> I am just expecting ki hmlog hmesa aise hi khelte </p>
+    <p class="para16">masti karte hue rahe and dheer sara trips plan</p> 
+    <p class="para17"> kare...Road trips mostly...</p>
+    <p class="para18"> Now neeche dekho aapke lie kuch banaya hai... </p>
+   <p class="para19">  I know bhut accha ni hai but kaam chala lo...</p>
+   <p class="para20">  I am just getting started...Dheere dheere better</p>
+   <p class="para21"> hoga....Scroll dowwwwnnnnnnn....</p>
+   <p class="para22">Areee.....ni aaya kya....yahi to rakha tha...</p>
+   <p class="para23">Wait a minute...I think neeche girr gaya hai..</p>
+   <p class="para24"> Ruko na..uthane to doo....dekh lena dekh lena..</p>
+   <p class="para25">aapka hi hai..zada neeche gir gaya hai....</p>
+   <p class="para26">uthane do..</p>
+   <p class="para27">Gussaoo ni....utha liya...dekho abb..</p>
+   
+        <video  plays-inline class="video-clip"  id="video" controls width="120px" >
+              <source src="video.mp4" controls type="video/mp4" id="video"> 
+        </video>
+   
+    <script type="text/javascript">
+        const image = document.getElementById('video');
+        image.style.display = 'none';
+        setTimeout(() =>{
+            image.style.display='block';
+        },89000);
+        setTimeout(() =>{
+            image.classList.add('show')
+        },89000);
+
+    </script>
+    
+</body>
+</html>
+
+
